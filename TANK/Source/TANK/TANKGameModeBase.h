087f8ff1fe5935e5c2bca5ab766d0af3a5a5ee2c
Kuba <jakub.mleczak2000@gmail.com>
2018-02-13 19:57:52 +0100

    dzien 1.1

---
+++ TANK/Source/TANK/TANKGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TANKGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANK_API ATANKGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
