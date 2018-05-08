d1b8856231cc312dc2a643431c7166a5ae333be6
DESKTOP-H0JMURF\Kuba <jakub.mleczak2000@gmail.com>
2018-05-08 23:07:27 +0200

    8.05 Działa v2

--- TANK/Source/TANK/Private/TankAimComp.cpp
+++ TANK/Source/TANK/Private/TankAimComp.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-#include "Tank1.h"
 #include "TankAimComp.h"
+#include "Tank1.h"
+
 #include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
 
 
